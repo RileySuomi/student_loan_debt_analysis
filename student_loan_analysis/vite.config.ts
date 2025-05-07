79b929135844b8389d46a699a452eafaed4ad430
RileySuomi <suomir@spu.edu>
2025-05-06 22:43:20 -0700

    make sure all is commited

--- student_loan_analysis/vite.config.ts
+++ student_loan_analysis/vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+// import path from 'path';
 
 // new tailwindcss v4
 
 export default defineConfig({
-  base: '/student_loan_analysis/',
   plugins: [
     react(),
     tailwindcss(), 
-  ]
+  ],
+  base: '/student_loan_analysis/',
+  build: {
+    outDir: 'docs'  // Change output directory to "docs"
+  }
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, 'src'),
+  //   },
+  // },
 });
